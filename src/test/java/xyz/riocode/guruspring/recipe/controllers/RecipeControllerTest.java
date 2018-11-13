8cf8d16c81ea681d3a183d65efb482253e006e2b
Nikola <nikola.cvetkovic91@gmail.com>
2018-11-13 23:55:13 +0100

    add controller, implement method, implement test, add html page

---
+++ src/test/java/xyz/riocode/guruspring/recipe/controllers/RecipeControllerTest.java
@@ -0,0 +1,41 @@
+package xyz.riocode.guruspring.recipe.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import xyz.riocode.guruspring.recipe.domain.Recipe;
+import xyz.riocode.guruspring.recipe.services.RecipeService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController recipeController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void testShowRecipeById() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1l);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+
+}
