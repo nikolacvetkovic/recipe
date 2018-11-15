310a60796ac285391553360f93a6eca9591fd507
Nikola <nikola.cvetkovic91@gmail.com>
2018-11-15 15:19:46 +0100

    add method

--- src/main/java/xyz/riocode/guruspring/recipe/services/RecipeService.java
+++ src/main/java/xyz/riocode/guruspring/recipe/services/RecipeService.java
@@ -10,4 +10,5 @@ public interface RecipeService {
     Set<Recipe> getRecipes();
     Recipe findById(Long id);
     RecipeCommand saveRecipeCommand(RecipeCommand command);
+    RecipeCommand findCommandById(Long id);
 }
