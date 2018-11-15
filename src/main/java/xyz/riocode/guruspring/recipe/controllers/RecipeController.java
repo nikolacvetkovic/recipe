aeda8be36a5762ec68bffc100f136970d8122072
Nikola <nikola.cvetkovic91@gmail.com>
2018-11-15 15:19:17 +0100

    add method in controller for saving and updating, change path for show method

--- src/main/java/xyz/riocode/guruspring/recipe/controllers/RecipeController.java
+++ src/main/java/xyz/riocode/guruspring/recipe/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package xyz.riocode.guruspring.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import xyz.riocode.guruspring.recipe.commands.RecipeCommand;
 import xyz.riocode.guruspring.recipe.services.RecipeService;
 
 @Controller
@@ -15,10 +18,32 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
     }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/"+ savedRecipeCommand.getId() +"/show";
+    }
+
+    @RequestMapping("/recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/recipeform";
+    }
+
 }
