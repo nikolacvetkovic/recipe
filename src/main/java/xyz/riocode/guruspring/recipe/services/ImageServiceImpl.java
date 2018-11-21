64d0e18c75815515b8c2386d1a56c4fdf4060a2f
Nikola <nikola.cvetkovic91@gmail.com>
2018-11-22 00:06:43 +0100

    add controller, service and page for image upload

---
+++ src/main/java/xyz/riocode/guruspring/recipe/services/ImageServiceImpl.java
@@ -0,0 +1,12 @@
+package xyz.riocode.guruspring.recipe.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ImageServiceImpl implements ImageService {
+    @Override
+    public void saveImageFile(Long recipeId, MultipartFile multipartFile) {
+
+    }
+}
